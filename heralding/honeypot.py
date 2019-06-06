147a4008b279be1bfc6c5984cfc346911a53a02d
KavitaSah <46550973+KavitaSah@users.noreply.github.com>
2019-06-05 17:23:27 -0700

    Update honeypot.py

--- heralding/honeypot.py
+++ heralding/honeypot.py
@@ -194,5 +194,5 @@ class Honeypot:
         ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
         ssl_context.check_hostname = False
         ssl_context.load_cert_chain(pem_file)
-        ssl_context.set_ciphers('AES128-SHA256')        
+        ssl_context.set_ciphers('TLSv1.2:-DH:-ECDHE:-PSK:-NULL:-CAMELLIA')        
         return ssl_context
